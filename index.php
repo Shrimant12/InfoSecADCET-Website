304ba1ec9c6d80274bf6de2c856c41dfc9773903
Shrimant <smshrimant@gmail.com>
2019-03-26 01:14:05 +0530

    New file added

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+
+<html>
+<head>
+<title>InfoSecADCET - Offical page
+</title>
+</head>
+
+</body>
+</div>
+
+<br>
+<br>
+<br>
+<h1 align="center">InfoSecADCET</h1>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+Made with ❤ by SMshrimant
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+<a href="https://github.com/Shrimant12" target="_blank" ><IMG SRC="GitHub-Mark-64px.png" ALT="some text" WIDTH=40 HEIGHT=40> </a>
+
+<script>
+
+</script>
+
+</body>
+</html>
